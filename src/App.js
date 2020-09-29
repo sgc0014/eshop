c40a45270eddfefa7fbacf785b1cc76e88e74303
unknown <girisagar0014@gmail.com>
2020-09-29 21:39:48 +0545

    navbar start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from './logo.png'
+import {FiUser,FiSearch,FiShoppingCart,FiHeart} from 'react-icons/fi'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <navbar className="navbar">
+        <header className='brand-name'><img src={logo} /></header>
+        <ul className="first-part nav-items">
+          <li className="nav-item">Men</li>
+          <li className="nav-item">Women</li>
+          <li className="nav-item">Accesories</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+        <ul className="second-part nav-items">
+          <li className="nav-item" style={{background:"#e8e8e8",borderRadius:"6px",padding:"3px 11px", display:"flex",alignItems:"center"}}>
+            <input placeholder="search..." style={{background:"#e8e8e8",border:'none',outline:"none"}}></input>
+            <FiSearch size={'1.5em'}/>
+          </li>
+          <li className="nav-item">
+            <span><FiUser size={'1.5em'} /></span>
+          </li>
+          <li className="nav-item">
+            <span><FiHeart size={'1.5em'}/></span>
+            <span className='count'>0</span>
+          </li>
+          <li className="nav-item">
+            <span><FiShoppingCart size={'1.5em'}/></span>
+            <span className='count'>0</span>
+          </li>
+        </ul>
+      </navbar>
+      <section className="Advert-slideshow"></section>
     </div>
   );
 }
